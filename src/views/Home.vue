a31834c7b06117672f450af02f7e0926adf53d08
albinagossou <albinagossou@gmail.com>
2021-09-27 09:54:20 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,258 @@
+<template>
+  <v-container fluid class="main_container">
+    <v-img
+      contain
+      max-height="70"
+      max-width="200"
+      src="../../studynklogo.png"
+      width="10%"
+      height="100%"
+      class="logo_transition"
+    />
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="elevation-6 mt-10">
+          <v-window v-model="step">
+            <v-window-item :value="1">
+              <v-row>
+                <v-col cols="12" md="6">
+                  <v-card-text class="mt-12">
+                    <h4 class="text-center">Connectez-vous à votre compte</h4>
+                    <h6 class="text-center  grey--text ">Et apprenez</h6>
+                    <v-row align="center" justify="center">
+                      <v-col cols="12" sm="8">
+                        <v-text-field
+                          label="Email"
+                          outlined
+                          dense
+                          color="blue"
+                          autocomplete="false"
+                          class="mt-16"
+                        />
+                        <v-text-field
+                          label="Mot de Passe"
+                          outlined
+                          dense
+                          color="blue"
+                          autocomplete="false"
+                          type="password"
+                        />
+                        <v-row>
+                          <v-col cols="12" sm="7">
+                            <v-checkbox
+                              label="Se rappeler de moi"
+                              class="mt-n1"
+                              color="blue"
+                            >
+                            </v-checkbox>
+                          </v-col>
+                          <v-col cols="12" sm="5">
+                            <span class="caption blue--text"
+                              >Mot de passe oublié?</span
+                            >
+                          </v-col>
+                        </v-row>
+                        <v-btn color="blue" dark block tile>Connexion</v-btn>
+
+                        <h5 class="text-center  grey--text mt-4 mb-3">
+                          Connectez vous avec
+                        </h5>
+                        <div
+                          class="d-flex  justify-space-between align-center mx-10 mb-16"
+                        >
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="red">fab fa-google</v-icon>
+                          </v-btn>
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="blue">fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="light-blue lighten-3"
+                              >fab fa-twitter</v-icon
+                            >
+                          </v-btn>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-col>
+                <v-col cols="12" md="6" class="blue rounded-bl-xl">
+                  <div style="  text-align: center; padding: 180px 0;">
+                    <v-card-text class="white--text">
+                      <h3 class="text-center ">Pas encore de compte ?</h3>
+                      <h6 class="text-center">Créez en un ici</h6>
+                    </v-card-text>
+                    <div class="text-center">
+                      <v-btn tile outlined dark @click="step++"
+                        >Inscription</v-btn
+                      >
+                    </div>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-window-item>
+            <v-window-item :value="2">
+              <v-row>
+                <v-col cols="12" md="6" class="blue rounded-br-xl">
+                  <div style="  text-align: center; padding: 180px 0;">
+                    <v-card-text class="white--text">
+                      <h3 class="text-center ">Déjà inscrit?</h3>
+                      <h6 class="text-center">
+                        Connectez vous avec votre compte
+                      </h6>
+                    </v-card-text>
+                    <div class="text-center">
+                      <v-btn tile outlined dark @click="step--"
+                        >Connexion</v-btn
+                      >
+                    </div>
+                  </div>
+                </v-col>
+
+                <v-col cols="12" md="6">
+                  <v-card-text class="mt-12">
+                    <h4 class="text-center">Inscrivez vous</h4>
+                    <h6 class="text-center  grey--text ">
+                      Nous allons tout mettre en place pour que vous puissiez
+                      commencer votre <br />
+                      première expérience sur Studynk
+                    </h6>
+                    <v-row align="center" justify="center">
+                      <v-col cols="12" sm="8">
+                        <v-text-field
+                          label="Username"
+                          outlined
+                          dense
+                          color="blue"
+                          autocomplete="false"
+                        />
+                        <v-row>
+                          <v-col cols="12" sm="6">
+                            <v-text-field
+                              label="First Name"
+                              outlined
+                              dense
+                              color="blue"
+                              autocomplete="false"
+                              class="mt-4"
+                            />
+                          </v-col>
+                          <v-col cols="12" sm="6">
+                            <v-text-field
+                              label="Grade"
+                              outlined
+                              dense
+                              color="blue"
+                              autocomplete="false"
+                              class="mt-4"
+                            />
+                          </v-col>
+                        </v-row>
+                        <v-text-field
+                          label="Email"
+                          outlined
+                          dense
+                          color="blue"
+                          autocomplete="false"
+                        />
+                        <v-text-field
+                          label="Password"
+                          outlined
+                          dense
+                          color="blue"
+                          autocomplete="false"
+                          type="password"
+                        />
+                        <v-col cols="12">
+                          <v-menu
+                            ref="menu"
+                            v-model="menu"
+                            :close-on-content-click="false"
+                            :return-value.sync="date"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="auto"
+                          >
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field
+                                v-model="date"
+                                label="Votre date de naissance"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                              ></v-text-field>
+                            </template>
+                            <v-date-picker v-model="date" no-title scrollable>
+                              <v-spacer></v-spacer>
+                              <v-btn text color="primary" @click="menu = false">
+                                Cancel
+                              </v-btn>
+                              <v-btn
+                                text
+                                color="primary"
+                                @click="$refs.menu.save(date)"
+                              >
+                                OK
+                              </v-btn>
+                            </v-date-picker>
+                          </v-menu>
+                        </v-col>
+                        <v-row>
+                          <v-col cols="12" sm="7">
+                            <v-checkbox
+                              label="J'accepte les termes et conditions d'utilisations"
+                              class="mt-n1"
+                              color="blue"
+                            >
+                            </v-checkbox>
+                          </v-col>
+                          <v-col cols="12" sm="5">
+                            <span class="caption blue--text ml-n4"
+                              >Termes &Conditions</span
+                            >
+                          </v-col>
+                        </v-row>
+                        <v-btn color="blue" dark block tile
+                          >Inscrivez vous</v-btn
+                        >
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    step: 1,
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
+<style scoped>
+.v-application .rounded-bl-xl {
+  border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+  border-bottom-right-radius: 300px !important;
+}
+.main_container {
+  max-width: 100%;
+}
+</style>
